b171ede1036248e248971e4096b3e48b16f9b2c5
tgohil04 <tgohil@spartaglobal.co>
2017-06-02 16:04:34 +0100

    Config.ru and bands_controller.rb tested in Chrome and Postman.

--- controller/bands_controller.rb
+++ controller/bands_controller.rb
@@ -5,24 +5,31 @@ class BandsController < Sinatra::Base
 	end
 	# INDEX rest
 	get "/bands" do 
+		"<h1>Awesome Bands</h1>"
 	end
 	# NEW rest
 	get "/bands/new" do
+		"Insert new awesome band here."
 	end
 	# SHOW rest
 	get "/bands/:id" do 
+		"Return awesome band name: #{params[:id]}"
 	end
 	# CREATE rest
 	post "/bands" do 
+		"Create an awesome band"
 	end
 	# UPDATE rest
 	put "/bands/:id" do
+		"Update an awesome band: #{params[:id]}"
 	end
 	# DELETE rest
 	delete "/bands/:id" do
+		"Be horrible and delete an awesome band: #{params[:id]}"
 	end
 
 	# EDIT rest
 	get "/bands/:id/edit" do 
+		"Edit an awesome band: #{params[:id]}"
 	end
 end
